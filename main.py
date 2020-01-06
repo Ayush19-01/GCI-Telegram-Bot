5c43efdb057dd37d9a32e245fd3a7c014308123b
Ayush19-01 <57313104+Ayush19-01@users.noreply.github.com>
2020-01-06 14:04:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #Made for the sole purpose of GCI-2019
 from bot import fedorainfrabot
 import getforks
-import time
 
 bot = fedorainfrabot("config.cfg")
 
@@ -12,17 +11,25 @@ def make_reply(msg):
     elif msg =="hello" or msg=="Hello" or msg=="hi" or msg=="Hi":
         reply="Hi! How are you!?"
     elif msg=="/what":
-        reply="I can get you the number of forks for each repository of fedors-infra\nJust type /forks\nTo get forks of an individual repository try /fork1 where the number signifies the repository number(Available from 1 till 30)"
+        reply="I can get you the number of forks for each repository of fedors-infra\nJust type /forks\nTo get forks of an individual repository try /fork1 where the number signifies the repository number(Available from 1 till 30)\n Also you can type '/fork reponame' to get the forks for the same "
     elif msg=="/start":
         reply="Welcome to the Fedora-Infra Bot\nI am designed to fetch the number of forks for each repository of fedora-infra.\nJust type /forks\nFor more info type /what"
 
     elif msg[0:5]=="/fork" :
-        try:
-            reply=getforks.textno()
-            x=int(msg[5:])-1
-            reply="Forks for\n"+reply[x]
-        except:
-            reply="Not Found!"
+        if len(msg)>6:
+            try:
+                a=msg[6:]
+                tmp1=getforks.textdict()
+                reply="Forks for "+a+" :   "+ tmp1[a]+" forks"
+            except:
+                reply = "Not Found!"
+        else:
+            try:
+                reply=getforks.textno()
+                x=int(msg[5:])-1
+                reply="Forks for "+reply[x]
+            except:
+                 reply="Not Found!"
     else:
         reply="Sorry, that's not a valid command!\nTry /what to know more."
     return reply
@@ -42,4 +49,3 @@ while True:
             from_ = item["message"]["from"]["id"]
             reply = make_reply(message)
             bot.send_message(reply, from_)
-
